3d056b36ef6bb395e9fa8945831a2c7ccc6868db
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-04 12:22:03 +0300

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "task_5_rg"
+    storage_account_name  = "task5storageacctunique"
+    container_name        = "task5container"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "task_5_rg" {
+  name     = "task_5_rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "task_5_sa" {
+  name                     = "task5storageacctunique"
+  resource_group_name      = azurerm_resource_group.task_5_rg.name
+  location                 = azurerm_resource_group.task_5_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "task_5_container" {
+  name                  = "task5container"
+  storage_account_name  = azurerm_storage_account.task_5_sa.name
+  container_access_type = "private"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.task_5_rg.name
+}
