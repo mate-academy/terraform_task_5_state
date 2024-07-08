bfc07904849f0183d28d0dd20a5599779defb6fc
Iryna Lyakhova <ippk0629@gmail.com>
2024-07-08 11:40:47 +0300

    Fixed

--- main.tf
+++ main.tf
@@ -1,20 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.110.0"
-    }
-  }
   backend "azurerm" {
     resource_group_name  = "task_5_rg"
     storage_account_name = "storagetask5account"
     container_name       = "task5container"
     key                  = "terraform.tfstate"
   }
-}
-
-provider "azurerm" {
-  features {}
 }
 
 resource "azurerm_resource_group" "task_5_rg" {
