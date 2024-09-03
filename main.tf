d19390b78c2664a4e78267c2493b270c9a9c0504
oleksandrhaupt <o.haupt@steel.mil.ua>
2024-09-03 22:35:38 +0300

    update

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name   = "task_5_ah"
-    storage_account_name  = "storagetask5account"
-    container_name        = "task5container"
-    key                   = "terraform.tfstate"
-    use_oidc              = true
+    resource_group_name  = "task_5_ah"
+    storage_account_name = "storagetask5account"
+    container_name       = "task5container"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
