e0dc5ecabcce3737fe95beb07fc14412d1047623
Losiev <elijahlosiev7@gmail.com>
2024-10-31 20:30:52 +0200

    SOlution

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "tftask5stacc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
